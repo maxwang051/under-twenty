149cadaa589a74a41e089b356eac90a8deeef13c
Max Wang <maxwang051@gmail.com>
2017-07-27 08:49:34 -0700

    added stats page, still a bit weird

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -3,6 +3,35 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 	var begin = true;
 
 	$scope.times = [];
+	$scope.stats = {
+		average: {
+			label: 'avg',
+			value: '00:00.00'
+		},
+		best: {
+			label: 'best',
+			value: '00:00.00'
+		}
+	};
+	$scope.viewingStats = false;
+
+	$scope.getAverage = function() {
+		var sum = 0;
+		for (var i = 0; i < $scope.times.length; i++) {
+			console.log($scope.times[i].time);
+			sum += parseInt($scope.times[i].time);
+		}
+
+		var average = sum / $scope.times.length;
+
+		if (average) {
+			$scope.stats.average.value = formatTime(average);
+		}
+	}
+
+	$scope.toggleTab = function() {
+		$scope.viewingStats = !$scope.viewingStats;
+	}
 
 	$scope.$on('space up', function(e, args) {
 		if (begin) {
@@ -32,10 +61,28 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 		timerStarted = false;
 
 		$scope.times.unshift({
-			time: $scope.currentHundredths,
+			time: $scope.currentMinutes*60*100 + $scope.currentSeconds*100 + $scope.currentHundredths,
 			formatted: $scope.currentMinutes + ":" + $scope.currentSeconds + "." + $scope.currentHundredths
 		});
+
+		$scope.getAverage();
 		$scope.$apply();
+	}
+
+	var formatTime = function(time) {
+		var temp = time;
+		console.log(time);
+		var minutes = Math.floor(temp / 6000);
+		temp = temp % 6000;
+		var seconds = Math.floor(temp / 100);
+		temp = temp % 100;
+		var hundredths = Math.floor(temp);
+
+		var mString = minutes < 10 ? '0'+minutes : ''+minutes;
+		var sString = seconds < 10 ? '0'+seconds : ''+seconds;
+		var hString = hundredths < 10 ? '0'+hundredths : ''+hundredths;
+
+		return mString + ':' + sString + '.' + hString;
 	}
 
 });
